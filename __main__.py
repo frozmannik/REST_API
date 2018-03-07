d7294083fb9c8c3d56bafdc6912f489bc1cc5b1f
nick <frozmanik@gmail.com>
2018-03-07 15:13:49 +0200

    last version

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
+from flask_admin import Admin  # dont have enough time to make a nice gui with it ( use postman instead)
 
 import json
 import os
@@ -7,26 +8,35 @@ import os
 
 app = Flask(__name__)
 api = Api(app)
+admin = Admin(app)
 
+# storage of news ( could be replacsed by database )
 file_path = os.path.abspath(os.path.join('data.json'))
-
 with open(file_path) as f:
-    items = json.load(f) # data of json file
-
-
-
+    items = json.load(f)
 
 class Item(Resource):
-#    @jwt_required() # for authenticate
+
+    # parse a file with a checking of each field
+    parser = reqparse.RequestParser()
+    parser.add_argument('title',type = str, required=True, help ="This field cannot be empty")
+    parser.add_argument('summary',type = str, required=True, help ="This field cannot be empty")
+    parser.add_argument('content',type = str, required=True, help ="This field cannot be empty")
+    parser.add_argument('published_status',type = bool, required=True, help ="This field cannot be empty") # WHAT IS published status??? is it boolean value? if not Change it here
+    parser.add_argument('published_date',type = str, required=True, help ="This field cannot be empty") # using string just to make it simplier here late will be changed to datatime
+    parser.add_argument('category',type = str, required=True, help ="This field cannot be empty")
+    parser.add_argument('author',type = str, required=True, help ="This field cannot be empty")
+
     def get(self, title):
-        item = next(filter(lambda x: x['title'] == title,items ), None)
-        return {'item': item}, 200 if item else 404
+        item = next(filter(lambda x: x['title'] == title,items ), None)  # function to find specific field
+        return {'item': item}, 200 if item else 404  # if successfully added return status 200(OK) if not 404(NOT FOUND)
 
     def post(self, title):
         if next(filter(lambda x: x['title'] == title,items ), None): #if we found an item which is not none
-            return {'message': "An item with name {} already exist".format(title)}, 400
+            return {'message': "An item with name {} already exist".format(title)}, 400  # if item already exist don't post it and return 400(BAD request)
 
-        data = request.get_json()
+        #adding a item to db
+        data = Item.parser.parse_args()
         item = {'title': title,
         'summary':data['summary'],
         'content': data['content'],
@@ -34,29 +44,38 @@ class Item(Resource):
         'published_date': data['published_date'],
         'category': data['category'],
         'author': data['author'] }
-
-
         items.append(item)
+
         with open(file_path, 'w') as f:
             json.dump(items, f)
         return item, 201 # code 201 for returned ok
-
 
     def delete(self, title):
         global items
         items = list(filter(lambda x: x['title'] != title, items))
         with open(file_path, 'w') as f:
             json.dump(items, f)
-        return {'message': 'Item delete'}
+        return {'message': 'Item delete'}, 200
 
-    def put(self,title):  # WORK IS HERE
+    def put(self,title):  # update exsisting news
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['title'] == title, items), None)
+
         if item is None:
-            item = {'title': name, 'price': data['price']}
-            item.append(item)
+            item = {'title': title,
+            'summary':data['summary'],
+            'content': data['content'],
+            'published_status': data['published_status'],
+            'published_date': data['published_date'],
+            'category': data['category'],
+            'author': data['author'] }
+            items.append(item)
+
+            with open(file_path, 'w') as f:
+                json.dump(items, f)
         else:
-            item.update(data)
-        return item
+            item.update(data), 200 # 200 for OK , can be replaced on 201 for created, not a big deal here
+        return item, 201 # 201 for creted
 
 class ItemList(Resource):
     def get(self):
